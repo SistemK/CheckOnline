7700dcad8e9196503cbb9a67ef2762b774ab2e79
SistemK <SistemK1987@gmail.com>
2018-04-18 18:46:35 +0300

    Finish

---
+++ checkOnline.ps1
@@ -0,0 +1,15 @@
+﻿function CheckOnline 
+{
+    $DATE = Get-Date -UFormat "%H-%M-%S-%m-%d-%Y"
+    $check = TNC ya.ru -InformationLevel Quiet
+    if ($check -eq $True)
+        {sleep 1
+        continue}
+    Else 
+        {$DATE | Out-File D:\CheckOnline.txt -Append}
+}
+$i = 1
+for (;;$i++)
+{
+    CheckOnline
+}
